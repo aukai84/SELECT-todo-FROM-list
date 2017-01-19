e57aabb4bfffcae816ed63d587da3734422710b3
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-19 00:13:38 -1000

    finished step 6..

--- todo_app.sql
+++ todo_app.sql
@@ -6,15 +6,12 @@ CREATE DATABASE "todo_app";
 \c "todo_app";
 
 CREATE TABLE "tasks" (id serial PRIMARY KEY NOT NULL, title varchar(255) NOT NULL, description text NULL, created_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT now(), updated_at timestamp WITHOUT TIME ZONE NULL, completed boolean NOT NULL DEFAULT false);
-SELECT * FROM "tasks";
 ALTER TABLE "tasks" DROP COLUMN "completed";
-SELECT * FROM "tasks";
 ALTER TABLE "tasks" ADD COLUMN "completed_at" timestamp WITHOUT TIME ZONE DEFAULT NULL;
-SELECT * FROM "tasks";
 ALTER TABLE "tasks" ALTER COLUMN "updated_at" SET NOT NULL;
 ALTER TABLE "tasks" ALTER COLUMN "updated_at" SET DEFAULT now();
 INSERT INTO "tasks" VALUES (default, 'Study SQL', 'Complete this exercise', now(), now(), NULL);
+INSERT INTO "tasks" ("title", "description") VALUES ('Study PostgreSQL', 'Read all the documentation');
+SELECT "title" FROM "tasks" WHERE "completed_at" IS NULL;
+UPDATE "tasks" SET "completed_at" = now() WHERE "title" = 'Study SQL';
 SELECT * FROM "tasks";
-UPDATE "tasks" SET "title" = 'Study PostgreSQL', "description" = 'Read all the documentation';
-SELECT * FROM "tasks";
-DROP TABLE "tasks";
