140e8dfd2ca15f8a13ce81be05b59102022ba298
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-19 00:52:25 -1000

    refactored code to be more readable

--- todo_app.sql
+++ todo_app.sql
@@ -5,20 +5,63 @@ DROP DATABASE IF EXISTS "todo_app";
 CREATE DATABASE "todo_app";
 \c "todo_app";
 
-CREATE TABLE "tasks" (id serial PRIMARY KEY NOT NULL, title varchar(255) NOT NULL, description text NULL, created_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT now(), updated_at timestamp WITHOUT TIME ZONE NULL, completed boolean NOT NULL DEFAULT false);
+CREATE TABLE "tasks" (
+  id serial PRIMARY KEY NOT NULL,
+  title varchar(255) NOT NULL,
+  description text NULL,
+  created_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+  updated_at timestamp WITHOUT TIME ZONE NULL,
+  completed boolean NOT NULL DEFAULT false
+);
 ALTER TABLE "tasks" DROP COLUMN "completed";
 ALTER TABLE "tasks" ADD COLUMN "completed_at" timestamp WITHOUT TIME ZONE DEFAULT NULL;
 ALTER TABLE "tasks" ALTER COLUMN "updated_at" SET NOT NULL;
 ALTER TABLE "tasks" ALTER COLUMN "updated_at" SET DEFAULT now();
-INSERT INTO "tasks" VALUES (default, 'Study SQL', 'Complete this exercise', now(), now(), NULL);
-INSERT INTO "tasks" ("title", "description") VALUES ('Study PostgreSQL', 'Read all the documentation');
+INSERT INTO "tasks"
+VALUES (
+  default,
+  'Study SQL',
+  'Complete this exercise',
+  now(),
+  now(),
+  NULL
+);
+INSERT INTO "tasks" (
+  "title",
+  "description"
+)
+VALUES (
+  'Study PostgreSQL',
+  'Read all the documentation'
+ );
 SELECT "title" FROM "tasks" WHERE "completed_at" IS NULL;
 UPDATE "tasks" SET "completed_at" = now() WHERE "title" = 'Study SQL';
 SELECT "title", "description" FROM "tasks" WHERE "completed_at" IS NULL;
 SELECT * FROM "tasks" order by "created_at" DESC;
-INSERT INTO "tasks" ("title", "description") VALUES ('mistake 1', 'a test entry');
-INSERT INTO "tasks" ("title", "description") VALUES ('mistake 2', 'another test entry');
-INSERT INTO "tasks" ("title", "description") VALUES ('third mistake', 'another test entry');
+INSERT INTO "tasks" (
+  "title",
+  "description"
+)
+VALUES (
+  'mistake 1',
+  'a test entry'
+);
+INSERT INTO "tasks" (
+  "title",
+  "description"
+)
+VALUES (
+  'mistake 2',
+  'another test entry'
+);
+INSERT INTO "tasks" (
+  "title",
+  "description"
+)
+VALUES (
+  'third mistake',
+  'another test entry'
+);
 SELECT * FROM "tasks" WHERE "title" LIKE '%mistake%';
 DELETE FROM "tasks" WHERE "title" = 'mistake 1';
 SELECT "title", "description" FROM "tasks" WHERE "title" LIKE '%mistake%';
